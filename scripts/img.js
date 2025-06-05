b6dd47536bcb6119365c114fef713add3a7b35f5
bymorse98 <bymorse1@gmail.com>
2025-06-04 17:10:05 -0700

    Delete scripts directory

--- scripts/img.js
+++
@@ -1,6 +0,0 @@
-const enlargeImg = (this) => {
-    var modal = document.getElementById("myModal");
-    var modalImg = document.getElementById("fullPicture");
-
-    modalImg.src = this.getAttribute("src");
-}
