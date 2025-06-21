2e3b2bcab55e1f28d2adff25b10e3d255733c7e8
bymorse98 <bymorse1@gmail.com>
2025-06-21 12:06:35 -0700

    Update home.js

--- scripts/home.js
+++ scripts/home.js
@@ -4,3 +4,10 @@ var reelStill = document.getElementById("reelStill");
 reel.onplay = () => {
   reelStill.style.visibility = "hidden";
 };
+
+// Animation intro First time each session only
+if (sessionStorage.getItem('firstTimeThisSession') === null) {
+  sessionStorage.setItem('firstTimeThisSession', 'true');
+} else {
+  document.getElementById("introContainer").classList.add("hide");
+}
